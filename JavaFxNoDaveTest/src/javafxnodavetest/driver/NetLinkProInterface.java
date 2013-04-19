6f6c7c5eaf55daa4ffb4adf073cda7bbf29eb695
Alex <Alex@Alex-PC.lan>
2013-04-19 23:34:57 +0200

    rechten

--- JavaFxNoDaveTest/src/javafxnodavetest/driver/NetLinkProInterface.java
+++ JavaFxNoDaveTest/src/javafxnodavetest/driver/NetLinkProInterface.java
@@ -1,8 +1,21 @@
 /*
- Part of Libnodave, a free communication library for Siemens S7 300/400 via
- the MPI adapter 6ES7 972-0CA22-0XAC
- or  MPI adapter 6ES7 972-0CA33-0XAC
- or  MPI adapter 6ES7 972-0CA11-0XAC.
+ Part of Libnodave, a free communication libray for Siemens S7
+ 
+ (C) Thomas Hergenhahn (thomas.hergenhahn@web.de) 2005.
+
+ Libnodave is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Library General Public License as published by
+ the Free Software Foundation; either version 2, or (at your option)
+ any later version.
+
+ Libnodave is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU Library General Public License
+ along with this; see the file COPYING.  If not, write to
+ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  
 */
 package javafxnodavetest.driver;
 
